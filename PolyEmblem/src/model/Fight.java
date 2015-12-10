e858416c90b668483b6594880f6b3498c6e10193
Sacha Lhopital <sachalhopital5@gmail.com>
2015-12-10 11:59:21 +0100

    Implement Round

--- PolyEmblem/src/model/Fight.java
+++ PolyEmblem/src/model/Fight.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package model;
 
-/**
- *
- * @author p1504274
- */
+//Not sure if it's a correct implementation. 
+//See Yoan for debug :D 
 public class Fight {
     
+    private Round zeRound;
+    private Character player;
+    private Character aICharacter;
 }
