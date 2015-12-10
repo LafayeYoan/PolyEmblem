6dd22678b5b7ab3cbc099ce6663e9d4ee8a4382d
Sacha Lhopital <sachalhopital5@gmail.com>
2015-12-10 11:45:08 +0100

    Implement Controller

--- PolyEmblem/src/controller/Controller.java
+++ PolyEmblem/src/controller/Controller.java
@@ -1,8 +1,14 @@
 package controller;
 
+import model.Events;
 import model.Fight;
+import view.Menu;
 
 public class Controller {
+    
+    private Menu zeMenu;
+    private Character player;
+    private Events event;
     
     private Fight act() {
         //TODO
