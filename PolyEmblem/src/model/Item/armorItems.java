4ed18590cb978af91eb92162f408da994c63713d
YoanLafaye <yoyolane@gmail.com>
2015-12-14 13:12:33 +0100

    rename classes

--- PolyEmblem/src/model/Item/armorItems.java
+++ PolyEmblem/src/model/Item/armorItems.java
@@ -3,11 +3,11 @@ package model.Item;
 import java.util.Map;
 import model.Items;
 
-public class armorItems extends Items {
+public class ArmorItems extends Items {
     
     private int protection;
 
-    public armorItems(String name, int weight, int protection) {
+    public ArmorItems(String name, int weight, int protection) {
         super(name, weight, null);
         this.protection = protection;
     }
--- PolyEmblem/src/model/Item/weaponItems.java
+++ PolyEmblem/src/model/Item/weaponItems.java
@@ -3,12 +3,12 @@ package model.Item;
 import java.util.Map;
 import model.Items;
 
-public class weaponItems extends Items {
+public class WeaponItems extends Items {
     
     private int damage;
     private int heal;
 
-    public weaponItems(String name, int weight, int damage, int heal) {
+    public WeaponItems(String name, int weight, int damage, int heal) {
         super(name, weight, null);
         this.damage = damage;
         this.heal = heal;
