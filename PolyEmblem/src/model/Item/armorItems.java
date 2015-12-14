83acae3883bbb6a6dd621d837e2907e99cd3ac1a
Sacha Lhopital <sachalhopital5@gmail.com>
2015-12-14 09:07:12 +0100

    Add attributs on armorItems and weaponItems

--- PolyEmblem/src/model/Item/armorItems.java
+++ PolyEmblem/src/model/Item/armorItems.java
@@ -4,9 +4,12 @@ import java.util.Map;
 import model.Items;
 
 public class armorItems extends Items {
+    
+    private int protection;
 
-    public armorItems(String name, int weight, Map allEffects) {
-        super(name, weight, allEffects);
+    public armorItems(String name, int weight, int protection) {
+        super(name, weight, null);
+        this.protection = protection;
     }
     
 }
--- PolyEmblem/src/model/Item/weaponItems.java
+++ PolyEmblem/src/model/Item/weaponItems.java
@@ -4,9 +4,14 @@ import java.util.Map;
 import model.Items;
 
 public class weaponItems extends Items {
+    
+    private int damage;
+    private int heal;
 
-    public weaponItems(String name, int weight, Map allEffects) {
-        super(name, weight, allEffects);
+    public weaponItems(String name, int weight, int damage, int heal) {
+        super(name, weight, null);
+        this.damage = damage;
+        this.heal = heal;
     }
     
 }
