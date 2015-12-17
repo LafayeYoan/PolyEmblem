a64269077a37c98ec2e4b7efac4117cbee58a687
Sacha Lhopital <sachalhopital5@gmail.com>
2015-12-17 09:36:34 +0100

    update stucture Character
    remove maxDext

--- PolyEmblem/src/controller/Controller.java
+++ PolyEmblem/src/controller/Controller.java
@@ -5,9 +5,11 @@ import java.util.List;
 import model.Characteristic;
 import model.Effect;
 import model.Event.DiscoverPlaceEvent;
+import model.Event.FightEvent;
 import model.Events;
 import model.Fight;
 import model.Items.EdibleItem;
+import model.Level;
 import view.HUD;
 
 public class Controller {
@@ -34,6 +36,16 @@ public class Controller {
                 + "Vous vous jetez dessus et passez la porte pile attend. "
                 + "Une chance qu'il reste des tics tacs dans la boite...",
                 new EdibleItem("Boite de tic tac entamée", 1, new Effect(Characteristic.STRENGHT, 2, 0))));
+        
+        /*event.add(new FightEvent("En route vers Polytech, votre très chère école, "
+                + "vous vous retrouvez au milieu de tous les autres étudiants du campus "
+                + "de l'université Lyon 1 dans le Tram menant à Condorcet. "
+                + "\n Ils sentent la transpiration et l'alcool, ce qui ne vous gêne pas beaucoup... "
+                + "\n Serré contre la porte, vous distinguez un paquet de tic tac au sol."
+                + "Vous vous appretez à le saisir lorsque la voix suave du tram annonce votre arrêt :"
+                + "Vous vous jetez dessus et passez la porte pile attend. "
+                + "Une chance qu'il reste des tics tacs dans la boite...",
+                new Character("Charlie le Kaid", new Level(), 0, 10)));*/
         
         //TODO : add more events
         System.out.println("---------------------------------------------------");
--- PolyEmblem/src/model/Character.java
+++ PolyEmblem/src/model/Character.java
@@ -1,7 +1,6 @@
 
 package model;
 
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +12,6 @@ public abstract class Character {
     private Level level;
     private int maxWeight;
     private int maxHealth;
-    private int maxDext;
     
     private int actualLife;
     
@@ -25,12 +23,11 @@ public abstract class Character {
     protected List<Skill> skills;
     private List<Effect> effects;
 
-    public Character(String name,Level level, int maxWeight, int maxHealth, int maxDext) {
+    public Character(String name,Level level, int maxWeight, int maxHealth) {
         this.name = name;
         this.level = level;
         this.maxWeight = maxWeight;
         this.maxHealth = maxHealth;
-        this.maxDext = maxDext;
         this.allItems = new LinkedList<>();
         this.weapon = null;
         this.armor = null;
@@ -38,11 +35,10 @@ public abstract class Character {
         this.actualLife = this.calcMaxHealth();
     }
 
-    public Character(String name, int maxWeight, int maxHealth, int maxDext) {
+    public Character(String name, int maxWeight, int maxHealth) {
         this.name = name;
         this.maxWeight = maxWeight;
         this.maxHealth = maxHealth;
-        this.maxDext = maxDext;
         this.level = new Level();
         this.initCharacteristics();
         this.actualLife = this.calcMaxHealth();
