daa3debec83c4af172d406b59129865a8e37d192
loickpaccaud <loick.paccaud@gmail.com>
2015-12-17 09:38:04 +0100

    update struct

--- PolyEmblem/src/model/Items/EdibleItem.java
+++ PolyEmblem/src/model/Items/EdibleItem.java
@@ -1,12 +1,14 @@
 package model.Items;
 
+import java.util.List;
 import java.util.Map;
+import model.Effect;
 import model.Item;
 
 public class EdibleItem extends Item {
 
-    public EdibleItem(String name, int weight, Map allEffects) {
-        super(name, weight, allEffects);
+    public EdibleItem(String name, int weight, Effect... effects) {
+        super(name, weight, effects);
     }
     
     private EdibleItem consume() {
