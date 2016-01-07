b38fee456820a5360d682361d5444b4554ecd341
YoanLafaye <lafaye.yoan@hotmail.fr>
2016-01-07 09:02:34 +0100

    Add validator class

---
+++ PolyEmblem/src/utils/Validator.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.util.List;
+
+/**
+ *
+ * @author Darkos
+ */
+public class Validator {
+    
+    public static boolean checkRange(int val, int max, int min){
+        boolean valid = true;
+        if(val< min){
+            System.out.println("La valeur entrée doit être supérieure à "+ min+ ".");
+            valid = false;
+        }
+        if(val > max){
+            System.out.println("La valeur entrée doit être inférieure à "+ max+ ".");
+            valid = false;
+        }
+        return valid;
+    }
+    
+    public static boolean checkInList(Object val, List<Object> vals){
+        boolean valid = true;
+        if(!vals.contains(val)){
+            valid = false;
+            System.out.println("La valeur entrée doit être incluse dans:");
+            vals.stream().forEach((o) -> {
+                System.out.println("\t- "+ o.toString());
+            });
+        }
+        return valid;
+    }
+    
+    public static boolean checkEmpty(String txt){
+        boolean valid = true;
+        if(txt.isEmpty()){
+            valid = false;
+            System.out.println("La chaine de caractère entrée est vide.");
+        }
+        return valid;
+    }
+}
